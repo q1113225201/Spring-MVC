a2110656995107ded9a19652ef7c14d3dd2dcfd7
林zero <1113225201@qq.com>
2019-03-05 22:48:31 +0800

    mybatis

--- src/main/java/com/sjl/springmvc/controller/UserController.java
+++ src/main/java/com/sjl/springmvc/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.sjl.springmvc.controller;
 
+import com.sjl.springmvc.dao.DbUserMapper;
 import com.sjl.springmvc.domain.UserBean;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
@@ -18,6 +21,7 @@ import java.util.Date;
 @Controller
 public class UserController {
     private static final Log logger = LogFactory.getLog(UserController.class);
+    DbUserMapper dbUserMapper;
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String getUsers(HttpServletRequest httpServletRequest, Model model) {
@@ -37,29 +41,30 @@ public class UserController {
 
     /**
      * 先于所有请求
+     *
      * @param username
      * @param password
      * @param model
      */
     @ModelAttribute
-    public void userModel(String username,String password,Model model){
-        logger.info("userModel:"+username+","+password);
+    public void userModel(String username, String password, Model model) {
+        logger.info("userModel:" + username + "," + password);
         UserBean userBean = new UserBean();
         userBean.setUsername(username);
         userBean.setPassword(password);
-        model.addAttribute("user",userBean);
-        model.addAttribute("username",username);
-        model.addAttribute("password",password);
+        model.addAttribute("user", userBean);
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
     }
 
-    @RequestMapping(value = "/user/login", method = {RequestMethod.POST,RequestMethod.GET})
+    @RequestMapping(value = "/user/login", method = {RequestMethod.POST, RequestMethod.GET})
     public String login(Model model) {
         UserBean userBean = (UserBean) model.asMap().get("user");
         userBean.setPassword("******");
-        if("1".equalsIgnoreCase(userBean.getUsername())) {
+        if ("1".equalsIgnoreCase(userBean.getUsername())) {
             //添加模型数据
             return "user";
-        }else {
+        } else {
             //添加模型数据
             return "index";
         }
